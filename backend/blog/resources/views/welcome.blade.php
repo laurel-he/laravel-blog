6cbcfed4df6d774b6e8d62f58ac0ba97788a854a
laurel-he <1570306409@qq.com>
2018-06-05 13:35:22 +0800

    初始化

--- backend/blog/resources/views/welcome.blade.php
+++ backend/blog/resources/views/welcome.blade.php
@@ -5,11 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>花花的个人博客</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <style>
             html, body {
@@ -79,16 +78,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ $name }}
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
