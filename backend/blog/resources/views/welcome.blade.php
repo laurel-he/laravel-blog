7494891d388dac2a4f09b2fc6c52a31b60577b42
laurel-he <1570306409@qq.com>
2018-06-29 14:38:22 +0800

    php7

--- backend/blog/resources/views/welcome.blade.php
+++ backend/blog/resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>花花的个人博客</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
         <!-- Styles -->
         <style>
             html, body {
@@ -78,9 +79,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    {{ $name }}
+                    Laravel
                 </div>
 
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
             </div>
         </div>
     </body>
--- backend/blog/routes/web.php
+++ backend/blog/routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('welcome',['name' => '花花']);
+    return view('welcome');
 });
