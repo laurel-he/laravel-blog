f86967b03975bbb6b6466f010f5d984d6e34c9b3
laurel-he <1570306409@qq.com>
2018-07-10 14:08:38 +0800

    add migrations

--- backend/blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('nickname');
+            $table->string('avatar');
+            $table->string('birthday');
+            $table->string('mark');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
